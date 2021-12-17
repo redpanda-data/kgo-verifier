be7f3dcefeb6d2beeb7b62fa2e97ac4968c115a2
John Spray <jcs@vectorized.io>
2021-12-17 14:13:54 +0000

    Sequential read, bad offsets

--- main.go
+++ main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	_ "net/http/pprof"
 	"os"
 	"runtime"
 	"strings"
 	"sync"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/twmb/franz-go/pkg/kerr"
 	"github.com/twmb/franz-go/pkg/kgo"
 	"github.com/twmb/franz-go/pkg/kmsg"
 	"github.com/twmb/franz-go/pkg/sasl/scram"
+	"golang.org/x/sync/semaphore"
 )
 
 func Die(msg string, args ...interface{}) {
@@ -33,6 +37,7 @@ func Chk(err error, msg string, args ...interface{}) {
 
 var (
 	debug    = flag.Bool("debug", false, "Enable verbose logging")
+	trace    = flag.Bool("trace", false, "Enable super-verbose (franz-go internals)")
 	brokers  = flag.String("brokers", "localhost:9092", "comma delimited list of brokers")
 	topic    = flag.String("topic", "", "topic to produce to or consume from")
 	username = flag.String("username", "", "SASL username")
@@ -40,10 +45,119 @@ var (
 	mSize    = flag.Int("msg_size", 16384, "Size of messages to produce")
 	pCount   = flag.Int("produce_msgs", 100000, "Number of messages to produce")
 	cCount   = flag.Int("consume_msgs", 100, "Number of validation reads to do")
+	seqRead  = flag.Bool("seq_read", true, "Whether to do sequential read validation")
 )
 
-func randomRead(nPartitions int32) {
+func sequentialRead(nPartitions int32) {
+	client := newClient(nil)
+	hwm := getOffsets(client, nPartitions, -1)
+	lwm := make([]int64, nPartitions)
 
+	for {
+		var err error
+		lwm, err = sequentialReadInner(nPartitions, lwm, hwm)
+		if err != nil {
+			log.Warnf("Restarting reader for error %v", err)
+			// Loop around
+		} else {
+			return
+		}
+	}
+}
+
+func sequentialReadInner(nPartitions int32, startAt []int64, upTo []int64) ([]int64, error) {
+	log.Infof("Sequential read...")
+
+	offsets := make(map[string]map[int32]kgo.Offset)
+	partOffsets := make(map[int32]kgo.Offset, nPartitions)
+	complete := make([]bool, nPartitions)
+	for i, o := range startAt {
+		partOffsets[int32(i)] = kgo.NewOffset().At(o)
+		log.Infof("Sequential start offset %s/%d %d...", *topic, i, partOffsets[int32(i)])
+		if o == upTo[i] {
+			complete[i] = true
+		}
+	}
+	offsets[*topic] = partOffsets
+
+	opts := []kgo.Opt{
+		kgo.ConsumePartitions(offsets),
+	}
+	client := newClient(opts)
+
+	last_read := make([]int64, nPartitions)
+
+	for {
+		fetches := client.PollFetches(context.Background())
+
+		var r_err error
+		fetches.EachError(func(t string, p int32, err error) {
+			log.Debugf("Sequential fetch %s/%d e=%v...", t, p, err)
+			r_err = err
+		})
+
+		if r_err != nil {
+			return last_read, r_err
+		}
+
+		fetches.EachRecord(func(r *kgo.Record) {
+			log.Debugf("Sequential read %s/%d o=%d...", *topic, r.Partition, r.Offset)
+			if r.Offset > last_read[r.Partition] {
+				last_read[r.Partition] = r.Offset
+			}
+
+			if r.Offset >= upTo[r.Partition]-1 {
+				complete[r.Partition] = true
+			}
+
+			validateRecord(r)
+		})
+
+		any_incomplete := false
+		for i, c := range complete {
+			log.Debugf("complete[%d] %v", i, c)
+
+			if !c {
+				any_incomplete = true
+			}
+
+		}
+
+		if !any_incomplete {
+			break
+		}
+	}
+
+	return last_read, nil
+}
+
+func validateRecord(r *kgo.Record) {
+	expect_key := fmt.Sprintf("%06d.%018d", 0, r.Offset)
+	log.Debugf("Consumed %s on p=%d at o=%d", r.Key, r.Partition, r.Offset)
+	if expect_key != string(r.Key) {
+
+		// Check bad offset list
+		vm := LoadValidMap()
+		ignore_offset := false
+		for _, o := range vm.badOffsets {
+			if o.P == r.Partition && o.O == r.Offset {
+				ignore_offset = true
+				break
+			}
+		}
+
+		if !ignore_offset {
+			Die("Bad read at offset %d on partition %s/%d.  Expect '%s', found '%s'", r.Offset, *topic, r.Partition, expect_key, r.Key)
+		} else {
+			log.Infof("Ignoring read validation at known-bad offset %s/%d %d", *topic, r.Partition, r.Offset)
+		}
+	} else {
+		log.Debugf("Read OK (%s) on p=%d at o=%d", r.Key, r.Partition, r.Offset)
+
+	}
+}
+
+func randomRead(nPartitions int32) {
 	// Basic client to read offsets
 	client := newClient(make([]kgo.Opt, 0))
 	startOffsets := getOffsets(client, nPartitions, -2)
@@ -90,14 +204,7 @@ func randomRead(nPartitions int32) {
 						if r.Partition != p {
 							Die("Wrong partition %d in read at offset %d on partition %s/%d", r.Partition, r.Offset, *topic, p)
 						}
-						expect_key := fmt.Sprintf("%06d.%018d", 0, r.Offset)
-						log.Debugf("Consumed %s on p=%d at o=%d", r.Key, r.Partition, r.Offset)
-						if expect_key != string(r.Key) {
-							Die("Bad read at offset %d on partition %s/%d.  Expect '%s', found '%s'", r.Offset, *topic, r.Partition, expect_key, r.Key)
-						} else {
-							log.Infof("Read OK (%s) on p=%d at o=%d", r.Key, r.Partition, r.Offset)
-
-						}
+						validateRecord(r)
 					}
 
 				}
@@ -121,7 +228,25 @@ func newRecord(producerId int, sequence int64) *kgo.Record {
 	return r
 }
 
+// Try to get offsets, with a retry loop in case any partitions are not
+// in a position to respond.  This is useful to avoid terminating if e.g.
+// the cluster is subject to failure injection while workload runs.
 func getOffsets(client *kgo.Client, nPartitions int32, t int64) []int64 {
+	wait_t := 2 * time.Second
+	for {
+		result, err := getOffsetsInner(client, nPartitions, t)
+		if err != nil {
+			log.Warnf("Retrying getOffsets in %v", wait_t)
+			time.Sleep(wait_t)
+		} else {
+			return result
+		}
+
+	}
+}
+
+func getOffsetsInner(client *kgo.Client, nPartitions int32, t int64) ([]int64, error) {
+	log.Infof("Loading offsets for topic %s t=%d...", *topic, t)
 	pOffsets := make([]int64, nPartitions)
 
 	req := kmsg.NewPtrListOffsetsRequest()
@@ -138,22 +263,73 @@ func getOffsets(client *kgo.Client, nPartitions int32, t int64) []int64 {
 	req.Topics = append(req.Topics, reqTopic)
 
 	resp, err := req.RequestWith(context.Background(), client)
-	Chk(err, "unable to request partition metadata: %v", err)
+	if err != nil {
+		log.Warnf("unable to request topic %s metadata: %v", *topic, err)
+		return nil, err
+	}
+	var r_err error
 	for _, partition := range resp.Topics[0].Partitions {
 		if partition.ErrorCode != 0 {
-			Die("Error %s getting partition %d metadata", kerr.ErrorForCode(partition.ErrorCode), partition.Partition)
+			log.Warnf("error fetching %s/%d metadata: %v", *topic, partition.Partition, kerr.ErrorForCode(partition.ErrorCode))
+			r_err = kerr.ErrorForCode(partition.ErrorCode)
 		}
 		pOffsets[partition.Partition] = partition.Offset
 		log.Debugf("Partition %d offset %d", partition.Partition, pOffsets[partition.Partition])
 	}
 
-	return pOffsets
+	return pOffsets, r_err
+}
+
+type ValidMap struct {
+	badOffsets []BadOffset
+}
+
+func LoadValidMap() ValidMap {
+	data, err := ioutil.ReadFile("bad_offsets.json")
+	if err != nil {
+		// Pass, assume it's not existing yet
+	}
+	var all_bad_offsets []BadOffset
+	if len(data) > 0 {
+		err = json.Unmarshal(data, &all_bad_offsets)
+		Chk(err, "Bad JSON %v", err)
+	}
+
+	return ValidMap{badOffsets: all_bad_offsets}
 }
 
 func produce(nPartitions int32) {
+	n := int64(*pCount)
+	for {
+		n_produced, bad_offsets := produceInner(n, nPartitions)
+		n = n - n_produced
+
+		if len(bad_offsets) > 0 {
+			log.Warnf("Produce stopped early, %d still to do", n)
+			log.Warnf("Storing bad offsets...")
+			vm := LoadValidMap()
+			all_bad_offsets := vm.badOffsets
+			all_bad_offsets = append(all_bad_offsets, bad_offsets...)
+			data, err := json.Marshal(all_bad_offsets)
+			err = ioutil.WriteFile("bad_offsets.json", data, 0644)
+			Chk(err, "Bad Write")
+		}
+
+		if n <= 0 {
+			return
+		}
+	}
+}
+
+type BadOffset struct {
+	P int32
+	O int64
+}
+
+func produceInner(n int64, nPartitions int32) (int64, []BadOffset) {
 	opts := []kgo.Opt{
 		kgo.DefaultProduceTopic(*topic),
-		kgo.MaxBufferedRecords(4096),
+		kgo.MaxBufferedRecords(1024),
 		kgo.ProducerBatchMaxBytes(1024 * 1024),
 		kgo.ProducerBatchCompression(kgo.NoCompression()),
 		kgo.RequiredAcks(kgo.AllISRAcks()),
@@ -165,25 +341,61 @@ func produce(nPartitions int32) {
 
 	var wg sync.WaitGroup
 
-	log.Infof("Producing %d messages (%d bytes)", *pCount, *mSize)
-	for i := 0; i < *pCount; i = i + 1 {
+	errored := false
+	produced := int64(0)
+
+	// Channel must be >= concurrency
+	bad_offsets := make(chan BadOffset, 16384)
+	concurrent := semaphore.NewWeighted(4096)
+
+	log.Infof("Producing %d messages (%d bytes)", n, *mSize)
+	for i := int64(0); i < n && len(bad_offsets) == 0; i = i + 1 {
+		concurrent.Acquire(context.Background(), 1)
+		produced += 1
 		var p = rand.Int31n(nPartitions)
-		r := newRecord(0, nextOffset[p])
+
+		expect_offset := nextOffset[p]
+		nextOffset[p] += 1
+
+		r := newRecord(0, expect_offset)
 		r.Partition = p
 		wg.Add(1)
 
 		log.Debugf("Writing partition %d at %d", r.Partition, nextOffset[p])
 		handler := func(r *kgo.Record, err error) {
+			concurrent.Release(1)
+			Chk(err, "Produce failed!")
+			if expect_offset != r.Offset {
+				// Shouldn't happen unless idempotence fails to do its thing
+				//Die("Produced at unexpected offset %d (expected %d) on partition %d", r.Offset, expect_offset, r.Partition)
+				log.Warnf("Produced at unexpected offset %d (expected %d) on partition %d", r.Offset, expect_offset, r.Partition)
+				bad_offsets <- BadOffset{r.Partition, r.Offset}
+				errored = true
+				log.Debugf("errored = %b", errored)
+			} else {
+				log.Debugf("Wrote partition %d at %d", r.Partition, r.Offset)
+			}
 			wg.Done()
-			Chk(err, "Oh no!")
-			log.Debugf("Wrote partition %d at %d", r.Partition, r.Offset)
 		}
 		client.Produce(context.Background(), r, handler)
-		nextOffset[p] += 1
 	}
-	client.Flush(context.Background())
-
 	wg.Wait()
+	close(bad_offsets)
+	if errored {
+		log.Warnf("%d bad offsets", len(bad_offsets))
+		var r []BadOffset
+		for o := range bad_offsets {
+			r = append(r, o)
+		}
+		if len(r) == 0 {
+			Die("No bad offsets but errored?")
+		}
+		successful_produced := produced - int64(len(r))
+		return successful_produced, r
+	} else {
+		wg.Wait()
+		return produced, nil
+	}
 }
 
 func newClient(opts []kgo.Opt) *kgo.Client {
@@ -199,6 +411,10 @@ func newClient(opts []kgo.Opt) *kgo.Client {
 	opts = append(opts,
 		kgo.SeedBrokers(strings.Split(*brokers, ",")...))
 
+	if *trace {
+		opts = append(opts, kgo.WithLogger(kgo.BasicLogger(os.Stderr, kgo.LogLevelDebug, nil)))
+	}
+
 	client, err := kgo.NewClient(opts...)
 	Chk(err, "Error creating kafka client")
 	return client
@@ -207,7 +423,7 @@ func newClient(opts []kgo.Opt) *kgo.Client {
 func main() {
 	flag.Parse()
 
-	if *debug {
+	if *debug || *trace {
 		log.SetLevel(log.DebugLevel)
 	} else {
 		log.SetLevel(log.InfoLevel)
@@ -237,9 +453,17 @@ func main() {
 	log.Debugf("Targeting topic %s with %d partitions", *topic, nPartitions)
 
 	// Prepare: write out several segments
-	produce(nPartitions)
+	if *pCount > 0 {
+		produce(nPartitions)
+	}
+
+	if *seqRead {
+		sequentialRead(nPartitions)
+	}
 
 	// Main stage: continue to write, while running random reader in background
-	randomRead(nPartitions)
+	if *cCount > 0 {
+		randomRead(nPartitions)
+	}
 
 }
