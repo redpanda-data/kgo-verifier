db7e81182f66db2b56755e6c96a860252c58ec73
Nicolae Vartolomei <nv@redpanda.com>
2023-11-17 15:48:39 +0000

    seq_read_worker: track and update lwm instead of last read
    Previous implementation led to reading messages twice on errors as we
    were using last_read as low watermark when resuming. Off-by-one error.
    
    There was another bug which is fixed now. If PollFetches returned only
    errors then sequentialReadInner would return an array with all offsets
    set to 0, thus causing a read from beginning.

--- pkg/worker/verifier/seq_read_worker.go
+++ pkg/worker/verifier/seq_read_worker.go
@@ -123,10 +123,11 @@ func (srw *SeqReadWorker) sequentialReadInner(startAt []int64, upTo []int64) ([]
 	if srw.config.rateLimitBytes > 0 {
 		rlimiter = rate.NewLimiter(rate.Limit(srw.config.rateLimitBytes), srw.config.rateLimitBytes)
 	}
-	last_read := make([]int64, srw.config.nPartitions)
+
+	lwm := append([]int64{}, startAt...)
 
 	for {
-		log.Debugf("Calling PollFetches (last_read=%v status %s)", last_read, srw.Status.Validator.String())
+		log.Debugf("Calling PollFetches (lwm=%v status %s)", lwm, srw.Status.Validator.String())
 		fetches := client.PollFetches(context.Background())
 		log.Debugf("PollFetches returned %d fetches", len(fetches))
 
@@ -139,8 +140,8 @@ func (srw *SeqReadWorker) sequentialReadInner(startAt []int64, upTo []int64) ([]
 		if r_err != nil {
 			// This is not fatal: server is allowed to return an error, the loop outside
 			// this function will try again, picking up from last_read.
-			log.Warnf("Returning on fetch error %v, read up to %v", r_err, last_read)
-			return last_read, r_err
+			log.Warnf("Returning on fetch error %v, lwm %v", r_err, lwm)
+			return lwm, r_err
 		}
 
 		fetches.EachRecord(func(r *kgo.Record) {
@@ -150,8 +151,10 @@ func (srw *SeqReadWorker) sequentialReadInner(startAt []int64, upTo []int64) ([]
 
 			log.Debugf("Sequential read %s/%d o=%d...", srw.config.workerCfg.Topic, r.Partition, r.Offset)
 			curReadCount += 1
-			if r.Offset > last_read[r.Partition] {
-				last_read[r.Partition] = r.Offset
+
+			// Increment low watermark to skip the message on next try.
+			if r.Offset >= lwm[r.Partition] {
+				lwm[r.Partition] = r.Offset + 1
 			}
 
 			if r.Offset >= upTo[r.Partition]-1 {
@@ -183,10 +186,10 @@ func (srw *SeqReadWorker) sequentialReadInner(startAt []int64, upTo []int64) ([]
 		}
 	}
 
-	log.Infof("Sequential read complete up to %v (validator status %v)", last_read, srw.Status.Validator.String())
+	log.Infof("Sequential read complete up to %v (validator status %v)", lwm, srw.Status.Validator.String())
 	client.Close()
 
-	return last_read, nil
+	return lwm, nil
 }
 
 func (srw *SeqReadWorker) ResetStats() {
